1ea15c00ec701b4bd37eb69ee181d47761bc0434
PAULO HENRIQUE MARGUTTI JUNIOR <paulo.hmjunior2@senacsp.edu.br>
2024-06-27 21:55:26 -0300

    alteracoes nos controllers, precisando arrumar cadastro de tipo de local

--- src/controller/TipoLocalController.js
+++ src/controller/TipoLocalController.js
@@ -3,27 +3,27 @@ const pool = require('../database/mysql');
 
 const TipoLocalController = {
     async criar(req, res) {
-        const {id, tipoLocal} = req.body;
+        const {nome_tipo} = req.body;
 
         const novoTipo = {
             id: TipoLocal[TipoLocal.length-1]?.id ? TipoLocal[TipoLocal.length-1]?.id+1 : 1,
             tipoLocal: tipo
         }
 
-        let sql = `INSERT INTO tipoLocal(tipoLocal) VALUES (?)`
-        const result = await pool.query(sql, [tipoLocal])
-        const indertId = result[0]?.insertId;
+        let sql = `INSERT INTO TipoLocal(nome_tipo, id_tipo) VALUES (?, ?)`
+        const result = await pool.query(sql, [nome_tipo, 1])
+        const insertId = result[0]?.insertId;
         if(!insertId)
             {
                 return res.status(401).json({message: 'erro ao criar tipo de local'})
             }
-        const sql_select = 'SELECT * FROM tipoLocal WHERE idTipoLocal = ?'
+        const sql_select = 'SELECT * FROM TipoLocal WHERE id_tipo = ?'
         const [rows] = await pool.query(sql_select, [insertId])
         return res.status(201).json(rows[0])
     },
 
     async listar(req, res) {
-        let sql = "SELECT * FROM tipoLocal";
+        let sql = "SELECT * FROM TipoLocal";
         const[rows] = await pool.query(sql);
 
         return res.status(200).json(rows);
@@ -34,14 +34,14 @@ const TipoLocalController = {
 
         const {tipoLocal} = req.body;
 
-        let sql = "UPDATE tipoLocal SET tipoLocal = ? WHERE idTipoLocal = ?"
+        let sql = "UPDATE tipoLocal SET TipoLocal = ? WHERE nome_tipo = ?"
         const result = await pool.query(sql, [tipoLocal, Number(paramId)])
         const changedRows = result[0]?.changedRows;
         if(!changedRows)
             {
                 return res.status(401).json({message: 'erro ao alterar tipo de local'})
             }
-        const sql_select = "SELECT * FROM tipoLocal WHERE idTipoLocal = ?"
+        const sql_select = "SELECT * FROM TipoLocal WHERE nome_tipo = ?"
         const [rows] = await pool.query(sql_select, [paramId])
 
         return res.status(201).json(rows[0]);
@@ -49,7 +49,7 @@ const TipoLocalController = {
 
     async show(req, res) {
         const paramId = req.params.id;
-        const sql_select = "SELECT * FROM tipoLocal WHERE idTipoLocal = ?"
+        const sql_select = "SELECT * FROM tipoLocal WHERE nome_tipo = ?"
         const [rows] = await pool.query(sql_select, Number(paramId))
 
         return res.status(201).json(rows[0])
@@ -58,7 +58,7 @@ const TipoLocalController = {
     async deletar(req, res){
         const paramId = req.params.id;
         
-        let sql = `DELETE FROM tipoLocal WHERE idTipoLocal = ?`
+        let sql = `DELETE FROM TipoLocal WHERE nome_tipo = ?`
         const result = await pool.query(sql, [Number(paramId)])
         const affectedRows = result[0]?.affectedRows;
         if(!affectedRows)
